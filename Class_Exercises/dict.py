dcde6e16f3d83f5f56608e866bc223ca9bd0ce9c
jaythaceo <jaythaceo@gmail.com>
2014-07-20 17:57:22 -0400

    Examples from class

--- Class_Exercises/dict.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-# Data types
-
-a = 23 # integer
-b = 3.14 # float
-c = "string" # string
-print c
---
+++ Class_Exercises/homework.py
@@ -0,0 +1,10 @@
+
+import math
+largest = None
+smallest = None
+
+while True:
+	num = raw_input("Enter a number: ")
+	if num == 'done': break
+        new_num  = max(num)
+        print new_nu
